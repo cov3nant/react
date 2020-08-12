5976b5f4bea906457c37f768076e6d5e61040881
unknown <agetorgodsway@gmail.com>
2020-08-12 10:56:24 +0100

    Updates

---
+++ precache-manifest.64e6d98aa44dfaf336cef3c4da2ee7eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108791cd6149e79521fcd38e0a9b0fd8",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "5eb107e7035b525355f7",
+    "url": "/react/static/css/main.8d5f2fab.chunk.css"
+  },
+  {
+    "revision": "aa6984cd07520a83fd66",
+    "url": "/react/static/js/2.0a57a6d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.0a57a6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb107e7035b525355f7",
+    "url": "/react/static/js/main.39f36628.chunk.js"
+  },
+  {
+    "revision": "e13f72c0a5b2117a7fa1",
+    "url": "/react/static/js/runtime-main.da8bee19.js"
+  },
+  {
+    "revision": "37145eccb37675a601eec1f724ae7077",
+    "url": "/react/static/media/IMG-20190403-WA0003.37145ecc.jpg"
+  },
+  {
+    "revision": "9bca9acbfa3091050467be5020025bf3",
+    "url": "/react/static/media/IMG-20190405-WA0003.9bca9acb.jpg"
+  },
+  {
+    "revision": "9fec9a83bef645fc306eebe612182ee2",
+    "url": "/react/static/media/IMG-20190714-WA0026.9fec9a83.jpg"
+  }
+]);
